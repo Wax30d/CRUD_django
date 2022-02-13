fa44b5f26e3a80e5f23103c79a70ef6619907289
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-02-13 19:30:02 +0500

    Model book updated.

--- book/models.py
+++ book/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Book(models.Model):
     name = models.CharField(max_length=50)
     picture = models.ImageField()
+    author = models.CharField(max_length=30, default="anonymous")
+    email = models.EmailField(blank=True)
+    describe = models.TextField(default="CRUD django tutorial")
+
+    def __str__(self):
+        return self.name
